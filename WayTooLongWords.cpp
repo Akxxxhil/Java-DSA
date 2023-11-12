574823cd4c348719b274d9bcdc81b2b05b92a60b
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2023-11-12 12:28:05 +0530

    2 problems

---
+++ WayTooLongWords.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+string newword (string s){
+    if(s.size()<=10){
+        return s;
+    }
+    else {
+        string ans=s[0]+to_string(s.size()-2)+s[s.size()-1];
+        return ans;
+    }
+
+}
+int main (){
+    int n;
+    cin>>n;
+for (int i=0;i<n;i++){
+    string s;
+    cin>>s;
+
+    string output=newword(s);
+    cout<<output<<endl;
+}
+    
+
+
+}
