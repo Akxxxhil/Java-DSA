42892298141b73439297dd93119d7df9956d7f8c
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2024-06-10 18:56:08 +0530

    basics Added first and added Last Linked List

--- Linked List/Basic.java
+++ Linked List/Basic.java
@@ -1,5 +1,50 @@
 public class Basic {
+    public static class Node {
+        int data;
+        Node next;
+
+        //constructor on Node
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+
+    public void addFirst(int data){
+        // step;=craete a new Node
+        Node newNode=new Node(data);
+
+        if(head==null){
+            head=tail=newNode;
+            return;
+        }
+
+        //step;=making th head point towards this new nodes next
+        newNode.next=head;
+
+        //step:= marking new node as head
+        head=newNode;
+    }
+
+    public void addLast(int data){
+        Node newNode=new Node(data);
+        if(head==null){
+            head=tail=newNode;
+        }
+        tail.next=newNode;
+        tail=newNode;
+    }
+
     public static void main(String[] args) {
-        System.out.println("hello Linked List");
+        //creation of new linked list
+        Basic ll=new Basic();
+        ll.addFirst(1);
+        ll.addFirst(2);
+        ll.addLast(3);
+        ll.addLast(4);
+
+
     }
 }
